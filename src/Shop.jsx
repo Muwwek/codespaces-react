546bb000731c49e082c6cb16800bca79a14e6a22
Adisak Suethong <adisak.sue@gmail.com>
2025-07-27 03:12:20 +0000

    Add Shop component with styling and book filtering functionality

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
+import './Shop.css';
+
 export default function Shop() {
-    const booklist =[
+    const books =[
         { "id": 1, "title": "The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About", "author": "Mel Robbins", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL381_SR381,381_.jpg", "price": 11.69 },
         { "id": 2, "title": "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips", "author": "Dr. Nicole Apelian", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91-E86oM2IL._AC_UL381_SR381,381_.jpg", "price": 37 },
         { "id": 3, "title": "Seven Things You Can't Say About China", "author": "Tom Cotton", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81+mN748qkL._AC_UL381_SR381,381_.jpg", "price": 19.58 },
@@ -8,11 +11,29 @@ export default function Shop() {
         { "id": 6, "title": "I Wish Someone Had Told Me . . .: The Best Advice for Building a Great Career and a Meaningful Life", "author": "Dana Perino", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81AOHbxGNfL._AC_UL254_SR254,254_.jpg", "price": 20.30 },
         { "id": 7, "title": "How to Giggle: A Guide to Taking Life Less Seriously", "author": "Hannah Berner", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81rO3vvG1mL._AC_UL254_SR254,254_.jpg", "price": 20.29 },
         { "id": 8, "title": "Strangers in Time: A World War II Novel", "author": "David Baldacci", "image_url": "https://images-na.ssl-images-amazon.com/images/I/816QI0pfuRL._AC_UL254_SR254,254_.jpg", "price": 17.84 }
-    ]
+    ];
+
+    const filteredBooks = books.filter((b) => b.price > 20);
+
+    const booklist = filteredBooks.map((b) => 
+        <div className='book' key={b.id}>
+            <img src={b.image_url} />
+            <div>
+                {b.title} <br />
+                Price: ${b.price} <br />
+                {b.author}
+            </div>
+        </div>
+    );
 
     return (
-        <div className="shop-container">
-            <h1>Welcome to the Shop</h1>
+        <div className="card1">
+            <div className='header'>
+                <h1>Welcome to the Shop</h1>
+            </div>
+            <div className="bl">
+                {booklist}
+            </div>
             <p>Here you can find a variety of products to purchase.</p>
             <button className="shop-button">Browse Products</button>
         </div>
